d0affd59d008dc8e5fc6dffce7b63ef71b8f27f3
irmawan9004 <irmawan9001@gmail.com>
2021-11-08 23:29:28 +0700

    i

--- src/elements/Stars/index.js
+++ src/elements/Stars/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-
 import propTypes from "prop-types";
-
 import "./index.scss";
+
 export default function Star({ className, value, height, width, spacing }) {
   const decimals = Number(value) % 1;
 
